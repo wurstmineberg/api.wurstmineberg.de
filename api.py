045401d4330a0ceb607505f32417854911b94af3
Fenhl <fenhl@fenhl.net>
2014-12-18 21:32:24 +0000

    Add an endpoint for grouped player stats

--- api.py
+++ api.py
@@ -276,6 +276,21 @@ def api_player_data(player_minecraft_name):
         nbtfile = os.path.join(config('serverDir'), config('worldName'), 'playerdata', uuid + '.dat')
     return nbtfile_to_dict(nbtfile)
 
+@app.route('/player/:player_id/stats-grouped.json')
+def api_player_stats_grouped(player_id):
+    """Returns the player's stats formatted as JSON with stats grouped into objects by category"""
+    stats = api_stats(player_id)
+    ret = {}
+    for stat_name, value in stats.items():
+        parent = ret
+        key_path = stat_name.split('.')
+        for key in key_path[:-1]:
+            if key not in parent:
+                parent[key] = {}
+            parent = parent[key]
+        parent[key_path[-1]] = value
+    return ret
+
 @app.route('/player/:player_minecraft_name/stats.json')
 def api_stats(player_minecraft_name):
     """Returns the stats JSON file from the server, also accepts the player id instead of the Minecraft name"""
