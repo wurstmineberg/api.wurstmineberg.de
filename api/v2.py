0f5ea4952d2302bb82b282ce169c4054ce5d88aa
Fenhl <fenhl@fenhl.net>
2015-09-05 11:43:07 +0000

    Fix region file path construction

--- api/v2.py
+++ api/v2.py
@@ -226,7 +226,7 @@ def api_chunk_column_overworld(world, x, z):
     """Returns the given chunk column in JSON-encoded <a href="http://minecraft.gamepedia.com/Anvil_file_format">Anvil</a> NBT."""
     import anvil
     world = minecraft.World(world)
-    region = anvil.Region(world.path / world / 'region' / 'r.{}.{}.mca'.format(x // 32, z // 32))
+    region = anvil.Region(world.path / world.name / 'region' / 'r.{}.{}.mca'.format(x // 32, z // 32))
     chunk_column = region.chunk_column(x, z)
     return api.util2.nbt_to_dict(chunk_column.data)
 
