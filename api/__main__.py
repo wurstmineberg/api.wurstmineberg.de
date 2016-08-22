6ca5d0a22c4f48d1ced56453d5fb6728a7a5ff35
Lothar Komas <thomaskolar90@gmail.com>
2016-08-22 22:19:33 +0200

    correct import of version

--- api/__main__.py
+++ api/__main__.py
@@ -11,8 +11,7 @@ import api.v1
 import api.v2
 
 try:
-    import version
-    __version__ = version.version
+    from api.version import version as __version__
 except ImportError:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
