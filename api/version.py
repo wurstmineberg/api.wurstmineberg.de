1f8b0094e1cd9401236260f02d55a96e871ad03c
Fenhl <fenhl@fenhl.net>
2016-11-30 19:02:21 +0000

    Provide fallback for version

--- api/version.py
+++ api/version.py
@@ -1,8 +1,9 @@
+import contextlib
 
 __version__ = None
 try:
     from api._version import version as __version__
 except ImportError:
-    from setuptools_scm import get_version
-    __version__ = get_version(root='..', relative_to=__file__)
-
+    with contextlib.suppress(ImportError, LookupError):
+        from setuptools_scm import get_version
+        __version__ = get_version(root='..', relative_to=__file__)
