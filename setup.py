376fc1bbb68c83380ebfe5135d271fbe9507aed8
Fenhl <fenhl@fenhl.net>
2017-06-01 18:52:10 +0000

    Add dependencies to setup.py and add .travis.yml

--- setup.py
+++ setup.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(name='api.wurstmineberg.de',
-      description='',
-      author='Wurstmineberg',
-      author_email='mail@wurstmineberg.de',
-      packages=["api"],
-      use_scm_version = {
-            "write_to": "api/_version.py",
-          },
-      setup_requires=["setuptools_scm"],
-      package_data={"api": ["assets/*.json"]}
-     )
-
+setuptools.setup(
+    name='api.wurstmineberg.de',
+    description='',
+    author='Wurstmineberg',
+    author_email='mail@wurstmineberg.de',
+    packages=["api"],
+    package_data={"api": ["assets/*.json"]},
+    use_scm_version = {
+        "write_to": "api/_version.py"
+    },
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        'NBT',
+        'Pillow',
+        'anvil',
+        'backuproll',
+        'bottle',
+        'minecraft',
+        'mcstatus',
+        'more-itertools',
+        'people',
+        'playerhead',
+        'requests',
+        'setuptools-scm'
+    ],
+    dependency_links=[
+        'git+https://github.com/wurstmineberg/python-anvil.git#egg=anvil',
+        'git+https://github.com/wurstmineberg/minecraft-backuproll.git#egg=backuproll',
+        'git+https://github.com/wurstmineberg/systemd-minecraft.git#egg=minecraft',
+        'git+https://github.com/wurstmineberg/people.git#egg=people',
+        'git+https://github.com/wurstmineberg/playerhead.git#egg=playerhead'
+    ]
+)
